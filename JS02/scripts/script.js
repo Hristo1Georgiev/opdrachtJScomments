ad703999cf712fdd5886f4d93848405a9ca5ce7a
Hristo Georgiev <83413781+Hristo1Georgiev@users.noreply.github.com>
2021-06-23 18:29:27 +0200

    Add files via upload

---
+++ JS02/scripts/script.js
@@ -0,0 +1,12 @@
+console.log("Hallo Winc Academy studenten")
+//"Dit is een grote som"
+let x = 1230941;
+let y = 1823794;
+let z = x * y;
+console.log(z)
+/*
+let a = 637263;
+let b = 54;
+let c = a / b;
+console.log(c)
+*/
